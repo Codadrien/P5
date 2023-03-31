7690162ea2f2de71fdfb7c8d6808335081b66ff3
Codadrien <adri1gonon@gmail.com>
2023-03-31 21:08:50 +0200

    up popUp

--- front/js/utils/popUp.js
+++ front/js/utils/popUp.js
@@ -2,7 +2,7 @@
 export function popUp(textPopUp, classPopUp) {
     const body = document.querySelector('body');
     const showPopup = document.createElement('p');
-    showPopup.innerHTML = `<p>${textPopUp}</p>`;
+    showPopup.textContent = textPopUp;
     body.append(showPopup);
     showPopup.classList.add(`${classPopUp}`);
     setTimeout(() => {
