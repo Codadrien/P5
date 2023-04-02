2e7c5f3268f0e98fc0fa69e9aad3ea69351cda4f
Codadrien <adri1gonon@gmail.com>
2023-04-02 18:05:31 +0200

    commentaire fonction api

--- front/js/utils/api.js
+++ front/js/utils/api.js
@@ -1,13 +1,16 @@
+// recupére les données de l'api
 export async function getProducts() {
     const response = await fetch("http://localhost:3000/api/products");
     return await response.json();
 }
 
+// recupére les données de l'api avec un paramètre
 export async function getProduct(id) {
     const response = await fetch("http://localhost:3000/api/products/" + id);
     return await response.json();
 }
 
+// envoie les données à l'api et recupère la réponse
 export async function sendOrder(contact, products) {
     const response = await fetch("http://localhost:3000/api/products/order", {
         method: 'POST',
